cc1746c64570af9862436a5cdbfe51ab664fc3fc
Michael3573 <126564353+Michael3573@users.noreply.github.com>
2023-10-26 19:05:51 +0200

    commit-02

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,10 +2,9 @@ import { ContactItem } from 'components/ContactItem/ContactItem';
 import style from './ContactList.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'Redux/operations';
-import { selectContacts, selectVisibleContacts } from 'Redux/selectors';
+import { selectVisibleContacts } from 'Redux/selectors';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
   const filteredContacts = useSelector(selectVisibleContacts);
   const dispatch = useDispatch();
 
